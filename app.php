aa2c5b75bc641d86a395230a51d329f7db853d93
RenardDeFeu <david.borg@le-campus-numerique.fr>
2022-10-07 10:16:09 +0200

    Instances

--- app.php
+++ app.php
@@ -3,6 +3,40 @@ require __DIR__ . '/vendor/autoload.php';
 
 use App\Animals\{Elephant,Zebra,Fish,BubbleFish,CatFish,ClownFish,Whale,Parrot,Dove};
 
-$animals = []
+$animals = array(
+    new Fish("Jean-Yves"), 
+    new Fish("Hervé Dumond"), 
+    new Fish("Jean-Claude Convenant"), 
+    new Fish("Orelsan"),
+    new Fish("Jean Reno"),
+    new BubbleFish("Tea"),
+    new BubbleFish("Coffee"),
+    new BubbleFish("Chartreuse"),
+    new CatFish("O'Malley"),
+    new CatFish("Duchesse"),
+    new ClownFish("Nemo"),
+    new Elephant("Hatti"),
+    new Elephant("Elmer"),
+    new Zebra("Code Barre"),
+    new Parrot("Pirate"),
+    new Parrot("Sideman"),
+    new Parrot("Carrot the parrot"),
+    new Parrot("Victor"),
+    new Parrot("Salma"),
+    new Parrot("Iago"),
+    new Parrot("Gérard"),
+    new Parrot("Mike Tyson"),
+    new Parrot("Elwood"),
+    new Parrot("Melmel"),
+    new Dove("Savon"),
+    new Dove("Pigeon")
+);
+
+foreach ($animals as $animal) {
+    echo $animal->getName();
+}
+
+
+
 
 ?>
--- src/Animal.php
+++ src/Animal.php
@@ -4,11 +4,17 @@ namespace App;
 
 abstract class Animal
 {
-    private $name;
+    private string $name;
+
+    //Caracteristics
+    protected bool $can_fly = false;
+    protected bool $can_walk = false;
+    protected bool $can_swim = false;
 
     public function __construct(string $name){
         $this->name = $name;
     }
+
     public function getName() :string
     {
         return $this->name;
